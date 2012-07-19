bfbeb9529b9d90d4b59e96d964e79467d22a0011
connaryscott <connaryscott@gmail.com>
2012-07-19 09:04:38 -0700

    fixed to no url mode

--- ldap/search.sh
+++ ldap/search.sh
@@ -3,7 +3,7 @@ DC="dc=dtolabs,dc=com"
 DN="cn=Manager,${DC}"
 PASSWORD="secret"
 
-LDAP_URL=ldap://10.70.13.107/
+#LDAP_URL=ldap://10.70.13.107/
 #LDAP_URL=ldap://localhost/
 
 if [ -n "${LDAP_URL}" ]
@@ -13,7 +13,6 @@ else
     H_ARG=
 fi
 
-#ldapsearch ${H_ARG} -w "${PASSWORD}" -b "${DC}" -x -D"${DN}"
+ldapsearch ${H_ARG} -w "${PASSWORD}" -b "${DC}" -x -D"${DN}"
 #ldapsearch ${H_ARG} -b "${DC}" -x 
-
-echo ldapsearch ${H_ARG} -b "${DC},ou=roles" -x 
+#echo ldapsearch ${H_ARG} -b "${DC},ou=roles" -x 
